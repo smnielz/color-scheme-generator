ad6c939425aa5a87f384f2353d11c459e28d6251
smnielz <110817166+smnielz@users.noreply.github.com>
2022-09-09 10:56:42 +0200

    Changes on html, css and js
    1. "copied" location
    2. number of colors

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const form = document.getElementById("form")
 const colotInput = document.getElementById("color-input")
 const menu = document.getElementById("menu")
-const btn = document.getElementById("btn")
 const colorMain = document.getElementById("color-main")
 const hexMain = document.getElementById("hex-main")
+let count = document.getElementById("count")
+const col = document.getElementsByClassName("col")
 
 function foo(i){
     let divs = document.getElementsByTagName('div')
-    document.getElementById("custom-tooltip").style.display = "inline";
+    let copiedText = document.getElementById("custom-tooltip")
+    copiedText.style.top = event.clientY
+    copiedText.style.left = event.clientX
+    copiedText.style.display = "inline";
     setTimeout( function() {
-        document.getElementById("custom-tooltip").style.display = "none";
+        copiedText.style.display = "none";
     }, 1000);
-    navigator.clipboard.writeText(divs[i+7].textContent.substring(0,7))
-
+    navigator.clipboard.writeText(divs[i+7].textContent)    
 }
 
-btn.addEventListener("click", function(e){  
+form.addEventListener("submit", function(e){  
     e.preventDefault() 
     let hex = colotInput.value.substr(1)
     let mode = menu.value
     colorMain.innerHTML = ""
     hexMain.innerHTML = ""
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=${mode}&count=5`,
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=${mode}&count=${count.value}`,
     {
         method: 'GET',
         headers: {
@@ -30,16 +33,12 @@ btn.addEventListener("click", function(e){
     }) 
         .then(res => res.json())
         .then(data => {
-            for(let i=0; i<5; i++){
+            for(let i=0; i<count.value; i++){
                 colorMain.innerHTML += `<div class="col color" onclick = "foo(${i})"></div>`
                 document.getElementsByClassName("color")[i].style.background = data.colors[i].hex.value
                 hexMain.innerHTML += `<div class="col white" onclick = "foo(${i})">${data.colors[i].hex.value}
-                </div>`              
-            }
-                        
+                </div>`   
+                col[i].style.width = `${100 / count.value}%`          
+            }                       
         })   
-})
-
-hexMain.addEventListener("click", function(){
-    
 })
